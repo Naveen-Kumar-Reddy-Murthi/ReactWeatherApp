7619ebdfbd61b6b194e93c72ca7af2e4e5139407
Naveen Reddy <mnreddy.nv@gmail.com>
2022-09-25 09:07:41 +0000

    Initial commit

---
+++ src/Api.jsx
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '2eb8b7524emsh54676a45bfaced6p1b6dedjsnad5bdd01e7d3',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6a018ddadf751e8f3358aefd1f73abdc";
---
+++ src/Search.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "./Api"; 
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+  
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+    console.log(searchData);
+  }
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
+  return (
+    <AsyncPaginate placeholder="Search for city" debounceTimeout={600} value={search}
+      onChage={handleOnChange} loadOptions={loadOptions} />
+
+  )
+}
+
+export default Search;
